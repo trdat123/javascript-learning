b490563ed8989ad68a64ca7044a69c2b3bbd12a1
trdat123 <nkokviper123@gmail.com>
2020-09-28 16:15:55 +0700

    add all file

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-name = "Dat";
-console.log(name);
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+function countdownBirthday() {
+    const currentDate = new Date();
+    const nextBirthday = new Date('16 April 2021');
+    //times left until birthday in miliseconds:
+    const timesLeft = nextBirthday - currentDate;
+    const totalSecondsLeft = Math.floor(timesLeft / 1000);
+    const daysLeft = Math.floor(totalSecondsLeft / 60 / 60 / 24);
+    const hoursLeft = Math.floor(totalSecondsLeft / 60 / 60) % 24
+    const minsLeft = Math.floor(totalSecondsLeft / 60) % 60;
+    const secondsLeft = Math.floor(totalSecondsLeft) % 60;
+    console.log(daysLeft, hoursLeft, minsLeft, secondsLeft);
+
+    daysEl.innerHTML = daysLeft;
+    hoursEl.innerHTML = hoursLeft;
+    minutesEl.innerHTML = minsLeft;
+    secondsEl.innerHTML = secondsLeft;
+}
+
+countdownBirthday();
+setInterval(countdownBirthday, 1000);
